bcf19ed2dff38483cb729396631b568f4219644c
bytesquad1 <pascal@bytesquad.tech>
2024-09-09 11:25:08 +0700

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import fastify from 'fastify';
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
+import cors from '@fastify/cors';
 import movieRoutes from './routes/movie.route';
 import mongoose from 'mongoose';
 
+
 const app = fastify({ logger: true });
+
+app.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
 
 // Connect to MongoDB
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect( `${process.env.MONGO_URL}`, {});
+        await mongoose.connect(`${process.env.MONGO_URL}`, {});
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MongoDB connection error:', error);
